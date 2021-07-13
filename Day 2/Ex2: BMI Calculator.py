d752260029a56fdb72a0ea8cbc81b2c2c6f55bf1
Nishi Kashyap <76994658+Nishi-16-K@users.noreply.github.com>
2021-07-13 16:51:39 +0530

    Create Ex2: BMI Calculator.py

---
+++ Day 2/Ex2: BMI Calculator.py
@@ -0,0 +1,4 @@
+height = input("enter your height in m: ")
+weight = input("enter your weight in kg: ")
+bmi=float(weight)/(float(height)**2)
+print(int(bmi)) #we will be printing output as a whole number, so we'll use int(bmi)
