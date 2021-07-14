78900092d6852b691898840b05d0737f18fb5f41
Nishi Kashyap <76994658+Nishi-16-K@users.noreply.github.com>
2021-07-14 21:49:46 +0530

    Create Ex4: Automated_Pizza_Order.py

---
+++ Day 3/Ex4: Automated_Pizza_Order.py
@@ -0,0 +1,21 @@
+print("Welcome to Python Pizza Deliveries!")
+size = input("What size pizza do you want? S, M, or L ")
+add_pepperoni = input("Do you want pepperoni? Y or N ")
+extra_cheese = input("Do you want extra cheese? Y or N ")
+bill = 0
+
+if size== "S":
+  bill = 15
+elif size == "M":
+  bill = 20
+elif size == "L":
+  bill = 25
+
+if add_pepperoni == "Y":
+  if size == "S":
+    bill = bill+2
+  else:
+    bill = bill+3
+if extra_cheese == "Y":
+  bill = bill+1
+print(f"pay {bill}")
