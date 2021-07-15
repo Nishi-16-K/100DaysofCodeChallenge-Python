2c393786095560f744bd205f10885736cdfc501d
Nishi Kashyap <76994658+Nishi-16-K@users.noreply.github.com>
2021-07-15 17:07:50 +0530

    Create Ex3: Treasure Map.py

---
+++ Day 4/Ex3: Treasure Map.py
@@ -0,0 +1,10 @@
+row1 = ["⬜️","⬜️","⬜️"]
+row2 = ["⬜️","⬜️","⬜️"]
+row3 = ["⬜️","⬜️","⬜️"]
+map = [row1, row2, row3]
+print(f"{row1}\n{row2}\n{row3}")
+position = input("Where do you want to put the treasure? ")
+col = int(position[0])
+ro = int(position[1])
+map[ro-1][col-1] = "X"
+print(f"{row1}\n{row2}\n{row3}")
