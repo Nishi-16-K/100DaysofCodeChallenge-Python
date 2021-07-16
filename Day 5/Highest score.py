8c22c09c5d41ae76297c0e9f90141f0957beecb2
Nishi Kashyap <76994658+Nishi-16-K@users.noreply.github.com>
2021-07-16 16:05:47 +0530

    Create Highest score.py

---
+++ Day 5/Highest score.py
@@ -0,0 +1,10 @@
+student_scores = input("Input a list of student scores ").split()
+for n in range(0, len(student_scores)):
+  student_scores[n] = int(student_scores[n])
+print(student_scores)
+
+maximum = 0
+for m in student_scores:
+  if m > maximum:
+   maximum = m
+print(f"The highest score in the class is: {maximum}")
