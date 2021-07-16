272c1fb218aac19a6d32e30cfac9bd231eb6fb1f
Nishi Kashyap <76994658+Nishi-16-K@users.noreply.github.com>
2021-07-16 16:34:52 +0530

    Create Add even.py

---
+++ Day 5/Add even.py
@@ -0,0 +1,12 @@
+total = 0
+for n in range(2,101,2):
+  total = total+n
+print(total)
+
+##Other method
+
+# total = 0
+# for n in range(1,101):
+#   if n %2==0:
+#     total=total+n
+# print(total)
