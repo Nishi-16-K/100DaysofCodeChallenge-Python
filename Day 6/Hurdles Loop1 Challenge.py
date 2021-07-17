ab2c07f5d47e5065276b539741c3e1364b25ea68
Nishi Kashyap <76994658+Nishi-16-K@users.noreply.github.com>
2021-07-17 18:36:14 +0530

    Create Hurdles Loop1 Challenge.py

---
+++ Day 6/Hurdles Loop1 Challenge.py
@@ -0,0 +1,19 @@
+#Reeborg's World Hurdle 1 Loop Challenge using for loop
+
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+def step():
+    move()
+    turn_left()
+    move()
+    turn_right()
+    move()
+    turn_right()
+    move()
+    turn_left()
+step()
+
+for in in range(0,5):
+    step()
