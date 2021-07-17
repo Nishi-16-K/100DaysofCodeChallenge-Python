66e7bc6ba91aa0c2e89e6be0e57e2ab7fc85187d
Nishi Kashyap <76994658+Nishi-16-K@users.noreply.github.com>
2021-07-17 18:37:02 +0530

    Create Ex2: Hurdles Loop2 Challenge.py

---
+++ Day 6/Ex2: Hurdles Loop2 Challenge.py
@@ -0,0 +1,20 @@
+#Reeborg's World Hurdle 2 Loop Challenge using while loop
+
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+def step():
+    move()
+    turn_left()
+    move()
+    turn_right()
+    move()
+    turn_right()
+    move()
+    turn_left()
+step()
+
+
+while at_goal() == False:
+    step()
