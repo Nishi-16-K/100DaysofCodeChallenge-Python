156a28a011f0ac3d3c524106c9c6e7b06d0a946e
Nishi Kashyap <76994658+Nishi-16-K@users.noreply.github.com>
2021-07-21 22:43:53 +0530

    Create PROJECT: Calculator.py

---
+++ Day-10/PROJECT: Calculator.py
@@ -0,0 +1,40 @@
+from art import logo
+
+def add(n1,n2):
+  return n1+n2
+def multiply(n1,n2):
+  return n1*n2
+def divide(n1,n2):
+  return n1/n2
+def subtract(n1,n2):
+  return n1-n2
+
+operations= {
+  "+" : add,
+  "-" : subtract,
+  "*" : multiply,
+  "/" : divide
+}
+def calc():
+  print(logo)
+  num1 = float(input("What's the first number: "))
+  num2 = float(input("What's the second number: "))
+  for i in operations:
+    print(i)
+
+  is_True = False
+  while not is_True:
+    opt = input("Pick an operation from the line above: ")
+    result = operations[opt](num1,num2)
+    print(f"{num1} {opt} {num2} = {result}")
+    user = input(f"Type 'y' to continue calcualting with {result}, or type 'n' to start new calculation: ")
+    if user == "n":
+      is_True = True
+      calc()
+    else:
+      num1 = result
+      
+calc()   
+    
+
+    
