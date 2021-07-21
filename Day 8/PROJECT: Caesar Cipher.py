3fdb4398f4b5eb1396d6bf40c4e1a51b4875becd
Nishi Kashyap <76994658+Nishi-16-K@users.noreply.github.com>
2021-07-21 22:52:53 +0530

    Create PROJECT: Caesar Cipher.py

---
+++ Day 8/PROJECT: Caesar Cipher.py
@@ -0,0 +1,47 @@
+import math
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+def caesar(t_text, shift_amount, direct):
+  l_text = ""
+  if direct == "decode":
+    shift_amount *= -1
+  for i in l_text:
+    if i in alphabet:    
+      position = alphabet.index(i)
+      new_position = position + shift_amount
+      l_text += alphabet[new_position]
+    else:
+      l_text = l_text+i
+  print(f"\nThe {direct}d result: {l_text}")
+
+logo = """           
+ ,adPPYba, ,adPPYYba,  ,adPPYba, ,adPPYba, ,adPPYYba, 8b,dPPYba,  
+a8"     "" ""     `Y8 a8P_____88 I8[    "" ""     `Y8 88P'   "Y8  
+8b         ,adPPPPP88 8PP"""""""  `"Y8ba,  ,adPPPPP88 88          
+"8a,   ,aa 88,    ,88 "8b,   ,aa aa    ]8I 88,    ,88 88          
+ `"Ybbd8"' `"8bbdP"Y8  `"Ybbd8"' `"YbbdP"' `"8bbdP"Y8 88   
+            88             88                                 
+           ""             88                                 
+                          88                                 
+ ,adPPYba, 88 8b,dPPYba,  88,dPPYba,   ,adPPYba, 8b,dPPYba,  
+a8"     "" 88 88P'    "8a 88P'    "8a a8P_____88 88P'   "Y8  
+8b         88 88       d8 88       88 8PP""""""" 88          
+"8a,   ,aa 88 88b,   ,a8" 88       88 "8b,   ,aa 88          
+ `"Ybbd8"' 88 88`YbbdP"'  88       88  `"Ybbd8"' 88          
+              88                                             
+              88           
+"""
+print(logo)
+
+s_continue = True 
+while s_continue:
+  direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:    ")
+  text = input("\nType your message:    ").lower()
+  shift = int(input("\nType the shift number:   "))
+
+  shift = shift % 26
+  caesar(t_text=text, shift_amount=shift, direct=direction)
+  result = input("Type 'yes' if you want to go again. Otherwise type 'no'")
+  if result == "no":
+    s_continue = False
+    print("Bye. See You")
