69661ded8ef43be5b57d8de2924ee35aee3a25e6
Nishi Kashyap <76994658+Nishi-16-K@users.noreply.github.com>
2021-08-23 23:25:31 +0530

    Create Automated B'day wishes.py

---
+++ Day-32/Automated B'day wishes.py
@@ -0,0 +1,21 @@
+import pandas
+import datetime as dt
+import random
+import smtplib
+
+today_tuple = (dt.datetime.now().month, dt.datetime.now().day)
+data = pandas.read_csv("birthdays.csv")
+birthdays_dict = {(data_row["month"], data_row["day"]): data_row for (index, data_row) in data.iterrows()}
+
+if today_tuple in birthdays_dict:
+    person = birthdays_dict[today_tuple]
+    file_path = f"letter_templates/letter_{random.randint(1,3).txt}"
+    with open(file_path) as letter_file:
+        contents = letter_file.read()
+        new= contents.replace("[NAME]", person["name"])
+
+    with smtplib.SMTP("smtp.gmail.com") as connection:
+        connection.starttls()
+        connection.login("nishikashyap2023@gmail.com", "pythonprogramming")
+        connection.sendmail(from_addr="nishikashyap2023@gmail.com", to_addrs=person["email"],
+                            msg=f"Subject: Happy Birthday!\n\n{new}")
