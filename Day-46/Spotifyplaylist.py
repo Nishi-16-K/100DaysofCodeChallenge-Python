3b4ed93e5fe664d50a71ee668bee0185b8aa4070
Nishi Kashyap <76994658+Nishi-16-K@users.noreply.github.com>
2022-04-24 00:43:59 +0530

    Update Spotifyplaylist.py

--- Day-46/Spotifyplaylist.py
+++ Day-46/Spotifyplaylist.py
@@ -7,8 +7,8 @@ from spotipy.oauth2 import SpotifyOAuth
 date = input("Which year do you want to travel to? Type the date in this format YYYY-MM-DD: ")
 response = requests.get("https://www.billboard.com/charts/hot-100/" + date)
 soup = BeautifulSoup(response.text, 'html.parser')
-song_names_spans = soup.find_all("span", class_="chart-element__information__song")
-song_names = [song.getText() for song in song_names_spans]
+song_names_spans = soup.find_all("h3", id="title-of-a-story", class_="a-no-trucate")
+song_names = [song.getText().strip('\n') for song in song_names_spans]
 
 #Spotify Authentication
 sp = spotipy.Spotify(
